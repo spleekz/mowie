8df8fe696a6a4740acc07df031f59775a0b5f770
spleekz <Shutnik-12K@yandex,ru>
2021-06-03 17:46:15 +0300

    added Redirect to main page from unknown page

--- src/components/MovieApp/MovieApp.jsx
+++ src/components/MovieApp/MovieApp.jsx
@@ -5,17 +5,20 @@ import SearchedMovieList from './SearchedMovieList/SearchedMovieList';
 import SearchForm from './SearchForm/SearchForm'
 import Movie from './Movie/Movie';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import './MovieApp.css'
 
 const MovieApp = () => {
   return (
     <div className='movie-app'>
       <SearchForm />
-      <Route exact path='/' render={() => <PopularMovieList />} />
-      <Route exact path='/movies/:searchedValue' component={() => <SearchedMovieList />} />
-      <Route path='/movie/:movieId' component={() => <Movie />} />
-      <Route path='/rated' component={() => <RatedMovies />} />
+      <Switch>
+        <Route exact path='/' render={() => <PopularMovieList />} />
+        <Route exact path='/movies/:searchedValue' component={() => <SearchedMovieList />} />
+        <Route path='/movie/:movieId' component={() => <Movie />} />
+        <Route path='/rated' component={() => <RatedMovies />} />
+        <Redirect to={'/'} />
+      </Switch>
     </div>
   );
 }
